694e4a7ca9d91be9fde75f3ab6d550676c8acf84
luciano martins <lcmartins@gmail.com>
2025-02-19 21:24:35 -0300

    fix: flyway

--- infrastructure/src/main/resources/db/migration/V1__create-database.sql
+++ infrastructure/src/main/resources/db/migration/V1__create-database.sql
@@ -1,22 +1,23 @@
-
 create table tbl_order(
         order_id varchar(36) not null primary key,
-        customer_id UNSIGNED BIGINT(19) not null,
-        total decimal(16, 5),
+        customer_id BIGINT(19) not null,
+        total decimal(16, 5) not null,
         created_time timestamp(6) not null,
-        update_time  timestamp(6)
+        updated_time  timestamp(6) null
 );
 
-create tbl_order_item(
+create table tbl_order_item(
     id varchar(36) not null primary key,
-    name varchar(250) not null,
-    price decimal(16, 5) not null,
-    order_id varchr(36) not null references tbl_order(order_id),
+    item_name varchar(250) not null,
+    item_price decimal(16, 5) not null,
+    order_id varchar(36) not null references tbl_order(order_id),
+    quantity int not null,
+    total decimal(16, 5) not null,
     created_time timestamp(6) not null
  );
 
 create table tbl_food(
-    food_id int not null primary key,
+    id int not null primary key,
     name varchar(255) not null,
     price decimal(16, 5) not null
 );
@@ -32,7 +33,5 @@ create table tbl_food_ingredient(
     ingredient_id int not null,
     constraint pk_food_ingredient primary key(food_id, ingredient_id),
     constraint fk_ingredient_id_ingredient foreign key(ingredient_id) references tbl_ingredient(ingredient_id),
-    constraint fk_food_id_food foreign key(food_id) references tbl_food(food_id)
+    constraint fk_food_id_food foreign key(food_id) references tbl_food(id)
 );
-
-
---
+++ infrastructure/src/main/resources/db/migration/V2__insert-domain.sql
@@ -0,0 +1,10 @@
+
+insert into tbl_ingredient(ingredient_id, name, quantity_in_stock) values(1, 'pao de brioche', 10);
+insert into tbl_ingredient(ingredient_id, name, quantity_in_stock) values(2, 'hamburger de picanha', 11);
+insert into tbl_ingredient(ingredient_id, name, quantity_in_stock) values(3, 'pickles', 11);
+insert into tbl_food(id, name, price) values(1, 'hamburger picanhoso', 30.55);
+insert into tbl_food(id, name, price) values(2, 'hamburger premium', 40.90);
+insert into tbl_food_ingredient values(1, 1);
+insert into tbl_food_ingredient values(1, 2);
+insert into tbl_food_ingredient values(2, 1);
+insert into tbl_food_ingredient values(2, 2);
